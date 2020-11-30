3f64b48b1119721301169cf1832b826c89f99807
Aninst <dranxin123@163.com>
2020-11-30 21:19:48 +0800

    start

---
+++ Simple_diedai_group.c
@@ -0,0 +1,14 @@
+// #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define MALLOC(type, n) (type *)malloc((n) * sizeof(type))
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+{
+    double *x = NULL, *x0 = NULL, e = 1e-8;
+
+    return 0;
+}
